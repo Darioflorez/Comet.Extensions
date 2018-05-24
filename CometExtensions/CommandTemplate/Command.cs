ed7446262dfa44178ccf183de8c4d65fac0d7e15
Dario Florez <florez.wilmar@gmail.com>
2018-05-24 21:47:29 +0200

    Add command template

---
+++ CometExtensions/CommandTemplate/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : ApplicationCommand
+    {
+        
+        public override void Execute()
+        {
+
+        }
+    }
+}
